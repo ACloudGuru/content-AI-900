be825db7aa885fa2da32e92b898b64eaef6aeba0
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2021-09-16 05:02:43 -0700

    removed install steps

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -8,21 +8,6 @@ param (
 [System.Environment]::SetEnvironmentVariable('azurecognitiveserviceskey', $key, [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('azurecognitiveserviceslocation', $location, [System.EnvironmentVariableTarget]::Machine)
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-choco install python --version=3.9.6 -y
-choco install git.install -y
-
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-Update-SessionEnvironment
-
-python -m pip install -U pip
-
-pip install azure-cognitiveservices-vision-computervision==0.9.0
-pip install azure-cognitiveservices-vision-face==0.5.0
-pip install azure-ai-textanalytics==5.1.0
-pip install Pillow==8.3.1
-
 cd C:\
 mkdir code
 cd code
